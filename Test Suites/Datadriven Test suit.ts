d6fa03791dec27d62f9ebe110efca22f5431aa3d
debanand <debanand@LNAR-PC051ULB.corp.capgemini.com>
2021-03-18 20:21:37 -0500

    My first commit for Katalon Studio

---
+++ Test Suites/Datadriven Test suit.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Datadriven Test suit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c0fde2b5-e54b-4b30-9ab1-b4b3dc9cc737</testSuiteGuid>
+   <testCaseLink>
+      <guid>4d52e39e-85c1-431d-a535-48b7592765f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/DataDriven test case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f535eaaa-5665-42ba-88d3-65269acd1a25</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Datadriven Login test data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>f535eaaa-5665-42ba-88d3-65269acd1a25</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>4809c531-a37a-43db-b741-f7313fd61c5b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f535eaaa-5665-42ba-88d3-65269acd1a25</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>b1dc6419-4749-4edf-85b5-2cd615a9cbc3</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Demo test suit.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo test suit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ba51ed0e-e128-433a-a9d1-08bfe78ce843</testSuiteGuid>
+   <testCaseLink>
+      <guid>a33a0471-401f-443b-8592-a99e743ec63e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Demo/ManualTestCase Demo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6ce20b5b-4b6d-4d6a-95a6-3b4accab4fdc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/Record Demo Test case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dfc855b4-63a6-4dfa-afaf-c1fad209bf80</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/Script Test case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
